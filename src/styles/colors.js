2b79b8b488b9522458925222ca9ac482347410e4
Nathaniel Perry <nathaniel.r.perry@gmail.com>
2020-12-23 10:12:13 -0600

    Added color pallete

---
+++ src/styles/colors.js
@@ -0,0 +1,10 @@
+const colors = {
+    light: '#ECECE9',
+    gray: '#A49E8D',
+    dark: '#564E58',
+    blue: '#058AB3',
+    green: '#759C45',
+    orange: '#FC440F',
+}
+
+export default colors;
