efce29da84fa2411df81e01a7e36c3c5ca5f1844
Nathaniel Perry <nathaniel.r.perry@gmail.com>
2020-12-23 10:06:26 -0600

    Removed default Gatsby styles and content

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `BlueGreenWebDev`,
+    description: `My name is Nathaniel. I am a full stack web developer with a passion for design.`,
+    author: `@bluegreenwizard`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
