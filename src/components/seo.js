b8338c8cb8cfeeea9f6dac1bd407c5da4b594f7e
Nathaniel Perry <nathaniel.r.perry@gmail.com>
2021-01-11 17:38:07 -0600

    Added meta image for social media

--- src/components/seo.js
+++ src/components/seo.js
@@ -52,6 +52,10 @@ function SEO({ description, lang, meta, title }) {
           content: `website`,
         },
         {
+          propert: `og:image`,
+          content: `/screenshot.png`,
+        },
+        {
           name: `twitter:card`,
           content: `summary`,
         },
